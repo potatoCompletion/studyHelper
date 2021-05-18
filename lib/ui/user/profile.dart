7f7daecadf803542fdc81e10d3f63f5609d163c7
YuJunhee <junheeyu.uni@gmail.com>
2021-05-18 16:25:15 +0900

    Change folder and file name

---
+++ lib/ui/user/profile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Profile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[850],
+    );
+  }
+}
---
+++ lib/ui/user/signup.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignUp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // background Color setup
+      backgroundColor: Colors.grey[850],
+
+      appBar: AppBar(
+        title: Text("Sign Up"),
+      ),
+    );
+  }
+}
