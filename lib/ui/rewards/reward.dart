020531c0b341804d02d8be116ae194c31e1d2784
YuJunhee <junheeyu.uni@gmail.com>
2021-05-18 11:33:27 +0900

    Change reward Page design

--- lib/ui/rewards/reward.dart
+++ lib/ui/rewards/reward.dart
@@ -5,17 +5,36 @@ class Reward extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('리워드 위젯 샘플')),
+        backgroundColor: Colors.grey[850],
+        appBar: AppBar(
+          backgroundColor: Colors.grey[850],
+          title: Text(
+            'Reward Collection',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 17.0,
+            ),
+          ),
+        ),
         body: GridView.count(
           crossAxisCount: 2,
           children: List.generate(
             100,
             (index) {
               return Center(
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(40, 70, 40, 70),
+                  color: Colors.grey[600],
                   child: Text(
-                'Item $index',
-                style: Theme.of(context).textTheme.headline6,
-              ));
+                    'Item $index',
+                    //style: Theme.of(context).textTheme.headline6,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              );
             },
           ),
         ));
