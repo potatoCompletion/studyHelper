7f0258471ddf3ca569a003ecee305447fb89de80
YuJunhee <junheeyu.uni@gmail.com>
2021-05-18 11:32:01 +0900

    Change setting Page design

--- lib/ui/setting/setting.dart
+++ lib/ui/setting/setting.dart
@@ -5,64 +5,162 @@ class Setting extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('설정 위젯 샘플')),
+      backgroundColor: Colors.grey[850],
+      // Scaffold element - appBar
+      appBar: AppBar(
+          backgroundColor: Colors.grey[850],
+          title: Text(
+            'Settings',
+            style: TextStyle(
+              fontSize: 17.0,
+              fontWeight: FontWeight.bold,
+            ),
+          )),
       body: ListView(
         padding: const EdgeInsets.all(11),
         children: <Widget>[
+          // 1st factor - Theme
           Container(
             height: 50,
-            color: Colors.amber[600],
-            child: const Center(child: Text('테마')),
+            color: Colors.grey[850],
+            child: const Center(
+                child: Text(
+              '테마',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            )),
           ),
+          // 2nd factor - Version Check
           Container(
             height: 50,
-            color: Colors.amber[600],
-            child: const Center(child: Text('버전 확인')),
+            color: Colors.grey[850],
+            child: const Center(
+                child: Text(
+              '버전 확인',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            )),
           ),
+          // 3rd factor - Profile setup
           Container(
             height: 50,
-            color: Colors.amber[600],
-            child: const Center(child: Text('프로필 설정')),
+            color: Colors.grey[850],
+            child: const Center(
+                child: Text(
+              '프로필 설정',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            )),
           ),
+          // 4th factor - User/Security
           Container(
             height: 50,
-            color: Colors.amber[600],
-            child: const Center(child: Text('개인/보안')),
+            color: Colors.grey[850],
+            child: const Center(
+                child: Text(
+              '개인/보안',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            )),
           ),
+          // 5th factor - Alarm
           Container(
             height: 50,
-            color: Colors.amber[600],
-            child: const Center(child: Text('알림')),
+            color: Colors.grey[850],
+            child: const Center(
+                child: Text(
+              '알림',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            )),
           ),
+          // 6th factor - Screen
           Container(
             height: 50,
-            color: Colors.amber[600],
-            child: const Center(child: Text('화면')),
+            color: Colors.grey[850],
+            child: const Center(
+                child: Text(
+              '화면',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            )),
           ),
+          // 7th factor - Others
           Container(
             height: 50,
-            color: Colors.amber[600],
-            child: const Center(child: Text('기타')),
+            color: Colors.grey[850],
+            child: const Center(
+                child: Text(
+              '기타',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            )),
           ),
+          // 8th factor - Help
           Container(
             height: 50,
-            color: Colors.amber[600],
-            child: const Center(child: Text('도움말')),
+            color: Colors.grey[850],
+            child: const Center(
+                child: Text(
+              '도움말',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            )),
           ),
+          // 9th factor - Information
           Container(
             height: 50,
-            color: Colors.amber[600],
-            child: const Center(child: Text('공지사항')),
+            color: Colors.grey[850],
+            child: const Center(
+                child: Text(
+              '공지사항',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            )),
           ),
+          // 10th factor - Contects
           Container(
             height: 50,
-            color: Colors.amber[600],
-            child: const Center(child: Text('고객센터')),
+            color: Colors.grey[850],
+            child: const Center(
+                child: Text(
+              '고객센터',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            )),
           ),
+          // 11th factor - Labotary
           Container(
             height: 50,
-            color: Colors.amber[600],
-            child: const Center(child: Text('실험실')),
+            color: Colors.grey[850],
+            child: const Center(
+                child: Text(
+              '실험실',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            )),
           ),
         ],
       ),
