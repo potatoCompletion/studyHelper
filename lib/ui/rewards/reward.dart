57cabaaab59487198bca06e7fb78cd9dc2e1f896
semiwo <semiwo0713@gmail.com>
2021-05-18 19:03:09 +0900

    reward yaml 추가

--- lib/ui/rewards/reward.dart
+++ lib/ui/rewards/reward.dart
@@ -1,42 +1,187 @@
 import 'package:flutter/material.dart';
+import 'package:scratcher/scratcher.dart';
 
-// Reward 위젯 구현
-class Reward extends StatelessWidget {
+class Reward extends StatefulWidget {
+  @override
+  _Reward createState() => _Reward();
+}
+
+class _Reward extends State<Reward> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.grey[850],
-        appBar: AppBar(
-          backgroundColor: Colors.grey[850],
-          title: Text(
-            'Reward Collection',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 17.0,
-            ),
-          ),
-        ),
-        body: GridView.count(
-          crossAxisCount: 2,
-          children: List.generate(
-            100,
-            (index) {
-              return Center(
-                child: Container(
-                  padding: EdgeInsets.fromLTRB(40, 70, 40, 70),
-                  color: Colors.grey[600],
-                  child: Text(
-                    'Item $index',
-                    //style: Theme.of(context).textTheme.headline6,
+    // TODO: implement build
+    return MaterialApp(
+      home: DefaultTabController(
+          length: 5,
+          child: Scaffold(
+              backgroundColor: Colors.grey[850],
+              appBar: AppBar(
+                  backgroundColor: Colors.grey[850],
+                  title: Text(
+                    '업적',
                     style: TextStyle(
-                      color: Colors.white,
+                      color: Colors.red,
+                      fontWeight: FontWeight.bold,
                       fontSize: 20.0,
                     ),
                   ),
+                  centerTitle: true,
+                  actions: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.settings),
+                      onPressed: () {
+                        print("hello");
+                      },
+                    ),
+                  ],
+                  bottom: TabBar(
+                    tabs: [
+                      Tab(
+                        text: 'GridView',
+                      ),
+                      Tab(
+                        text: 'GridView.Count',
+                      ),
+                      Tab(
+                        text: 'GridView.builder',
+                      ),
+                      Tab(
+                        text: 'GridView.custom',
+                      ),
+                      Tab(
+                        text: 'GridView.extent',
+                      ),
+                    ],
+                    isScrollable: true,
+                  )),
+              body: TabBarView(children: [
+                GridView(
+                  scrollDirection: Axis.vertical,
+                  controller: ScrollController(),
+                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: 200.0),
+                  children: List.generate(100, (index) {
+                    return Container(
+                      padding: EdgeInsets.all(20.0),
+                      child: Center(
+                        child: GridTile(
+                          footer: Text(
+                            'Item $index',
+                            textAlign: TextAlign.center,
+                          ),
+                          header: Text(
+                            'SubItem $index',
+                            textAlign: TextAlign.center,
+                          ),
+                          child: Icon(Icons.access_alarm,
+                              size: 40.0, color: Colors.white30),
+                        ),
+                      ),
+                      color: Colors.grey[600],
+                      margin: EdgeInsets.all(1.0),
+                    );
+                  }),
                 ),
-              );
-            },
-          ),
-        ));
+                GridView.count(
+                    crossAxisCount: 2,
+                    children: List.generate(100, (index) {
+                      return Container(
+                        padding: EdgeInsets.all(20.0),
+                        child: Center(
+                          child: GridTile(
+                            footer: Text(
+                              'Item $index',
+                              textAlign: TextAlign.center,
+                            ),
+                            header: Text(
+                              'SubItem $index',
+                              textAlign: TextAlign.center,
+                            ),
+                            child: Icon(Icons.access_alarm,
+                                size: 40.0, color: Colors.white30),
+                          ),
+                        ),
+                        color: Colors.grey[600],
+                        margin: EdgeInsets.all(1.0),
+                      );
+                    })),
+                GridView.builder(
+                  itemCount: 50,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3),
+                  itemBuilder: (BuildContext context, int index) {
+                    //if (index < 50)
+                    return Container(
+                      padding: EdgeInsets.all(20.0),
+                      child: Center(
+                        child: GridTile(
+                          footer: Text(
+                            'Item $index',
+                            textAlign: TextAlign.center,
+                          ),
+                          header: Text(
+                            'SubItem $index',
+                            textAlign: TextAlign.center,
+                          ),
+                          child: Icon(Icons.access_alarm,
+                              size: 40.0, color: Colors.white30),
+                        ),
+                      ),
+                      color: Colors.blue[400],
+                      margin: EdgeInsets.all(1.0),
+                    );
+                  },
+                ),
+                GridView.custom(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3),
+                    childrenDelegate:
+                        SliverChildListDelegate(List.generate(100, (index) {
+                      return Container(
+                        padding: EdgeInsets.all(20.0),
+                        child: Center(
+                          child: GridTile(
+                            footer: Text(
+                              'Item $index',
+                              textAlign: TextAlign.center,
+                            ),
+                            header: Text(
+                              'SubItem $index',
+                              textAlign: TextAlign.center,
+                            ),
+                            child: Icon(Icons.access_alarm,
+                                size: 40.0, color: Colors.white30),
+                          ),
+                        ),
+                        color: Colors.blue[400],
+                        margin: EdgeInsets.all(1.0),
+                      );
+                    }))),
+                GridView.extent(
+                  maxCrossAxisExtent: 200.0,
+                  children: List.generate(100, (index) {
+                    return Container(
+                      padding: EdgeInsets.all(20.0),
+                      child: Center(
+                        child: GridTile(
+                          footer: Text(
+                            'Item $index',
+                            textAlign: TextAlign.center,
+                          ),
+                          header: Text(
+                            'SubItem $index',
+                            textAlign: TextAlign.center,
+                          ),
+                          child: Icon(Icons.access_alarm,
+                              size: 40.0, color: Colors.white30),
+                        ),
+                      ),
+                      color: Colors.blue[400],
+                      margin: EdgeInsets.all(1.0),
+                    );
+                  }),
+                )
+              ]))),
+    );
   }
 }
