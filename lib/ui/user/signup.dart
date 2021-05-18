12b1cc8567b05ce35bca0365265942845f1eeac3
YuJunhee <junheeyu.uni@gmail.com>
2021-05-18 17:59:25 +0900

    Update SignUp Page

--- lib/ui/user/signup.dart
+++ lib/ui/user/signup.dart
@@ -4,21 +4,170 @@ class SignUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // background Color setup
       backgroundColor: Colors.grey[850],
-
       // Scaffold element -> appBar
       appBar: AppBar(
+        backgroundColor: Colors.grey[850],
         title: Text(
           "Sign Up",
           style: TextStyle(
-            color: Colors.white,
+            color: Colors.grey[300],
+            fontSize: 25.0,
             fontWeight: FontWeight.bold,
-            fontSize: 17.0,
           ),
         ),
-        backgroundColor: Colors.grey[850],
         elevation: 0.0,
+      ),
+      body: const SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 8.0),
+          child: RegisterForm(),
+        ),
+      ),
+    );
+  }
+}
+
+class RegisterForm extends StatefulWidget {
+  const RegisterForm({Key key}) : super(key: key);
+  @override
+  _RegisterFormState createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<RegisterForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _agreedToTOS = true;
+
+  bool _submittable() {
+    return _agreedToTOS;
+  }
+
+  void _submit() {
+    _formKey.currentState.validate();
+    print("Form submitted");
+  }
+
+  void _setAgreedToTOS(bool newValue) {
+    setState(() {
+      _agreedToTOS = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(
+            height: 35.0,
+          ),
+          TextFormField(
+            decoration: const InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.white,
+                    width: 1.0,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 1.0,
+                  ),
+                ),
+                prefixIcon: Icon(
+                  Icons.person,
+                  color: Colors.white,
+                ),
+                labelText: "Nickname",
+                labelStyle: TextStyle(
+                  color: Colors.white,
+                )),
+            validator: (String value) {
+              if (value.trim().isEmpty) {
+                return "Nickname is required";
+              }
+            },
+          ),
+          // Empty Box
+          const SizedBox(
+            height: 20.0,
+          ),
+          // Text Box
+          TextFormField(
+            decoration: const InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.white,
+                    width: 1.0,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.blue,
+                    width: 1.0,
+                  ),
+                ),
+                prefixIcon: Icon(
+                  Icons.email_outlined,
+                  color: Colors.white,
+                ),
+                labelText: "Email",
+                labelStyle: TextStyle(
+                  color: Colors.white,
+                )),
+            validator: (String value) {
+              if (value.trim().isEmpty) {
+                return "Email is required";
+              }
+            },
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: Row(
+              children: <Widget>[
+                Checkbox(
+                  value: _agreedToTOS,
+                  onChanged: _setAgreedToTOS,
+                ),
+                GestureDetector(
+                  onTap: () => _setAgreedToTOS(!_agreedToTOS),
+                  child: const Text(
+                    "I agree to the Terms of Servieces and Privacy Policy",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          Row(
+            children: <Widget>[
+              const Spacer(),
+              RaisedButton(
+                //highlightedBorderColor: Colors.blue,
+                color: Colors.grey[400],
+                onPressed: _submittable() ? _submit : null,
+                child: const Text(
+                  "Register",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
