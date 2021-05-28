0e6ccb3b23e445ad69bcb093f1689b721925d942
potatoCompletion <033kws2628@gmail.com>
2021-05-28 20:09:38 +0900

    2021-05-28 setting design fix by potatoCompletion

--- lib/ui/setting/setting_time.dart
+++ lib/ui/setting/setting_time.dart
@@ -16,57 +16,159 @@ class _TimeSettingState extends State<TimeSetting> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[850],
       appBar: AppBar(
+        brightness: Brightness.dark,
         title: Text('timer time setting'),
+        backgroundColor: Colors.grey[850],
       ),
-      body: ListView(
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      showModalBottomSheet(
-                          context: context,
-                          builder: (BuildContext builder) {
-                            return Container(
-                              height: MediaQuery.of(context).copyWith().size.height / 3,
-                              child: CupertinoTimerPicker(
-                                mode: CupertinoTimerPickerMode.hms,
-                                minuteInterval: 1,
-                                secondInterval: 1,
-                                initialTimerDuration: timerVal.dTimerTime,
-                                onTimerDurationChanged: (Duration changedTimer) {
+          Column(
+            children: [
+              Container(
+                width: 300,
+                height: 400,
+                child: ListView(children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.grey[800],
+                      ),
+                      onPressed: () {
+                        showModalBottomSheet(
+                            context: context,
+                            builder: (BuildContext builder) {
+                              return Container(
+                                height: MediaQuery.of(context)
+                                        .copyWith()
+                                        .size
+                                        .height /
+                                    3,
+                                child: CupertinoTimerPicker(
+                                  mode: CupertinoTimerPickerMode.hms,
+                                  minuteInterval: 1,
+                                  secondInterval: 1,
+                                  initialTimerDuration: timerVal.dTimerTime,
+                                  onTimerDurationChanged:
+                                      (Duration changedTimer) {
                                     timerVal.dTimerTime = changedTimer;
-                                },
-                              ),
-                            );
-                          });
-                    },
-                    child: Text('SELECT TIME'),
+                                  },
+                                ),
+                              );
+                            });
+                      },
+                      child: Text('SELECT TIME'),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.grey[800],
+                      ),
+                      onPressed: () {
+                        //     TODO: add function
+                      },
+                      child: Text('BLUETOOTH SETTING'),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.grey[800],
+                      ),
+                      onPressed: () {
+                        //     TODO: add function
+                      },
+                      child: Text('HELP'),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.grey[800],
+                      ),
+                      onPressed: () {
+                        //     TODO: add function
+                      },
+                      child: Text('PROFILE SETTING'),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.grey[800],
+                      ),
+                      onPressed: () {
+                        //     TODO: add function
+                      },
+                      child: Text('CONTACT'),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.grey[800],
+                      ),
+                      onPressed: () {
+                        //     TODO: add function
+                      },
+                      child: Text('UI/UX SETTING'),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.grey[800],
+                      ),
+                      onPressed: () {
+                        //     TODO: add function
+                      },
+                      child: Text('AUTHOR'),
+                    ),
+                  ),
+                ]),
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              Container(
+                width: 100,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context, timerVal.dTimerTime);
+                  },
+                  child: Text(
+                    '저장',
+                    style: TextStyle(fontSize: 20),
                   ),
                 ),
-                Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: 50,
-                  child: ElevatedButton(
-                    onPressed: () {
-                        Navigator.pop(context, timerVal.dTimerTime);
-                    },
-                    child: Text('저장'),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ]
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
